2ffb2d242f8cc9aae8d834f05b192cfbbf35965e
Jacob Oakley <jacoboakley21@gmail.com>
2016-09-29 23:53:50 +0000

    created fade for welcome page

--- assets/my-scripts.js
+++ assets/my-scripts.js
@@ -1,5 +1,18 @@
-var image = function() {
-  document.getElementById("hide").style.display='none';
-};
+var welcome = document.getElementById(shown);
+shown.style.opacity = 1;
+shown.style.transition = "opacity 3s";
 
-document.getElementById("button").onclick = image;
+var hiddenImage = document.getElementById(hidden);
+hidden.style.transition = "opacity 3s";
+
+function fade() {
+  shown.style.opacity = 0;
+  hidden.style.opacity = 1;
+}
+
+function unfade () {
+  shown.style.opacity = 1;
+  hidden.style.opacity = 0;
+}
+document.getElementById("button1").onclick = fade;
+document.getElementById("button2").onclick = unfade;
