ead4b866ff695ad5a10d926e22ec9e3a608fbf0f
Jacob Oakley <jacoboakley21@gmail.com>
2016-10-12 05:12:57 +0000

    fixed zindex issue

--- assets/my-scripts.js
+++ assets/my-scripts.js
@@ -1,18 +1,20 @@
-var welcome = document.getElementById(shown);
-shown.style.opacity = 1;
-shown.style.transition = "opacity 3s";
+var welcome = document.getElementById("shown");
+welcome.style.opacity = 1;
+welcome.style.transition = "opacity 3s";
 
-var hiddenImage = document.getElementById(hidden);
-hidden.style.transition = "opacity 3s";
+var hiddenImage = document.getElementById("hidden")
+hiddenImage.style.transition = "opacity 3s";
 
 function fade() {
-  shown.style.opacity = 0;
-  hidden.style.opacity = 1;
+  welcome.style.opacity = 0;
+  welcome.style.zIndex = -1;
+  hiddenImage.style.opacity = 1;
+  hiddenImage.style.zindex = 1;
 }
 
 function unfade () {
-  shown.style.opacity = 1;
-  hidden.style.opacity = 0;
+  welcome.style.opacity = 1;
+  hiddenImage.style.opacity = 0;
 }
 document.getElementById("button1").onclick = fade;
 document.getElementById("button2").onclick = unfade;
